475ba675af7f165613363f4835b9a279164e224c
GitHub Action <action@github.com>
2023-02-27 19:45:21 +0000

    Template cleanup

--- src/main/kotlin/com/github/divyanshdxn/jbplug/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/divyanshdxn/jbplug/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.divyanshdxn.jbplug.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.divyanshdxn.jbplug.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/divyanshdxn/jbplug/services/MyProjectService.kt
+++ src/main/kotlin/com/github/divyanshdxn/jbplug/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.divyanshdxn.jbplug.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.divyanshdxn.jbplug.MyBundle
 
 class MyProjectService(project: Project) {
 
